d0dd701ca8e86ccd3152c47142e8fd9e294bec89
meimingweili <yecanlee@yahoo.com>
2024-06-19 17:11:58 +0000

    [Update]tests folder with correct format

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,9 @@
 # The following three lines of codes show how to import the necessary modules to run the tests
-import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__name__), 'models')))
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__name__), 'models')))
 
 import pytest
 import torch
@@ -11,15 +13,19 @@ from torchvision.transforms import ToTensor
 from models.simple_unet import DiffusionUNet
 
 # Import a dataset from torchvision, then do some preprocessing, and then test the model
-dataset = torchvision.datasets.CIFAR10(root="./data", download=True, transform=ToTensor())
+dataset = torchvision.datasets.CIFAR10(root='./data',
+                                       download=True,
+                                       transform=ToTensor())
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=32)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 # Test initialization of the model
 def test_simple_unet():
     model = DiffusionUNet(image_channels=3, n_channels=64)
-    assert model is not None, "This model does not exist in the original file"
+    assert model is not None, 'This model does not exist in the original file'
+
 
 # Test the output of the model
 def test_model_output():
@@ -32,4 +38,4 @@ def test_model_output():
         with torch.inference_mode():
             x = x.to(device)
             out = model(x, t)
-            assert out.shape == x.shape, "The output shape is not the same as the input shape"
+            assert out.shape == x.shape, 'The output shape is not the same as the input shape'
