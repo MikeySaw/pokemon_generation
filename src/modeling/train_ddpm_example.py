eaa91fd741821d4a36b63cafb67248ef7eaa3e24
meimingweili <yecanlee@yahoo.com>
2024-06-21 00:31:27 +0000

    [IMPORTANT UPDATE]runnable train file with a lot of things.

--- src/modeling/train_ddpm_example.py
+++ src/modeling/train_ddpm_example.py
@@ -5,9 +5,11 @@ import sys
 
 import albumentations as A
 import torch
+import torch.nn as nn
 import torch.utils.data
 from torch.utils.data import Dataset
 import torchvision
+from torchvision import transforms
 import wandb
 from omegaconf import OmegaConf
 from PIL import Image  # noqa
@@ -30,6 +32,7 @@ from models.simple_ddpm import DenoiseDiffusion as ddf
 from models.simple_unet import DiffusionUNet  # noqa
 
 # Load the config file by using the OmegaConf library
+"""
 config = OmegaConf.load('../../conf/config.yaml')
 
 eps_pred_model = DiffusionUNet(image_channels=3, n_channels=64)
@@ -45,123 +48,123 @@ optimizer = torch.optim.Adam(eps_pred_model.parameters(),
 
 # Initialize the wandb
 wandb.init(project='diffusion', entity='yecanlee', config=config)
+"""
 
-
-def sample(config: OmegaConf):
-    """Sample images from the model, this should be a slower version of generating images the trick would be related to
-    the inference mode and torch.autocast."""
+# Define the sampling function
+def sample(config: OmegaConf, ddpm: ddf):
     with torch.no_grad():
-        # Another trick of profiling, we may use the .to(device) first then use "device=config.device" later to show faster speed
         x = torch.randn([
             config.training.n_samples, config.model.image_channels,
             config.dataset.image_size, config.dataset.image_size
-        ],
-                        device=config.device)  # noqa
+        ], device=config.device)
+        
         for t_ in tqdm(range(config.diffusion.n_steps), desc='Sampling'):
             t = config.diffusion.n_steps - t_ - 1
-            # Reverse pass denoising sampling process
-            x = diffusion.p_sample(
-                x,
-                x.new_full((config.training.n_samples, ), t, dtype=torch.long))
+            x = ddpm.p_sample(x, x.new_full((config.training.n_samples,), t, dtype=torch.long))
 
-        # Log samples to wandb
-        wandb.log({'generated_samples': [wandb.Image(sample) for sample in x]})
-        torchvision.utils.save_image(x, 'sample.png', nrow=4, normalize=True)
+    # Denormalize the images
+    x = (x + 1) / 2
+
+    # Log samples to wandb
+    wandb.log({'generated_samples': [wandb.Image(sample) for sample in x]})
+    torchvision.utils.save_image(x, f'sample_epoch_{wandb.run.step}.png', nrow=4, normalize=True)
 
 
-# define the training function
-def train(config: OmegaConf):
-    """
-    Train a model for denosing diffusion probabilistic models.
-    The core idea of this model would be add noise to the image at each step in 1000 steps, and then reverse the process
-    """
+# Define the training function
+def train(config: OmegaConf, ddpm: ddf, optimizer: torch.optim.Optimizer, train_data_loader: torch.utils.data.DataLoader):
+    total_loss = 0
+    for images, _ in train_data_loader:
+        images = images.to(config.device)
 
-    # Iterate through the dataset
-    for data in train_data_loader:
-        # Move data to device
-        data = data.to(config.device)
-
-        # Make the gradients zero
-        config.optimizer.zero_grad()
-        # Calculate loss
-        loss = ddf.loss(data)
-        # Compute gradients
+        optimizer.zero_grad()
+        loss = ddpm.loss(images)
         loss.backward()
-        config.optimizer.step()
-        wandb.log({'loss': loss.item()})
+        optimizer.step()
+
+        total_loss += loss.item()
+        wandb.log({'batch_loss': loss.item()})
+
+    return total_loss / len(train_data_loader)
 
 
-# define the inference function
-def run(config: OmegaConf):
-    """
-    Run one epoch of training and inference process for the DDPM model.
-    """
-    train_whole_loss = 0
-    for _ in tqdm(range(config.training.epochs), desc='Epochs'):
-        train(config)
-        sample(config)
+# define the training process wrapper
+def run(config: OmegaConf, eps_pred_model: nn.Module, optimizer: torch.optim.Optimizer, train_data_loader: torch.utils.data.DataLoader):
+    # Create an instance of DenoiseDiffusion
+    ddpm = ddf(eps_pred_model, config.device, config.diffusion.n_steps)
+
+    for epoch in tqdm(range(config.training.epochs), desc='Epochs'):
+        train_loss = train(config, ddpm, optimizer, train_data_loader)
+        wandb.log({'epoch': epoch, 'train_loss': train_loss})
+
+        if (epoch + 1) % config.training.sample_interval == 0:
+            sample(config, ddpm)
 
 
 # Define the image augmentation class
 class AlbumentationsTransform:
-    """
-    A wrapper class for albumentations augmentations.
-    """
-    def __init__(self, transforms):
+    def __init__(self, transforms: dict):
+        """
+        Args:
+            transforms (Dict): a dictionary of albumentation transforms
+        """
         self.transforms = A.Compose(transforms)
 
-    def __call__(self, image):
+    def __call__(self, image: torch.Tensor):
+        # first two lines were added to make sure no bug with a PIL image
+        transform_to_tensor = transforms.ToTensor()
+        image_tensor = transform_to_tensor(image)
+        image = image_tensor.permute(1, 2, 0).numpy()  # Convert to numpy array
         augmented = self.transforms(image=image)
         image = augmented['image']
-        return image
+        return torch.from_numpy(image).permute(2, 0, 1)  # Convert back to tensor
 
 
-# Define Custom Processor
-class CustomProcessor:
-    def __init__(self, processor):
-        self.processor = processor
-
-    def __call__(self, image_path, text, max_length=100):
-        image = Image.open(image_path).convert('RGB')
-        pixel_values = self.processor(image, return_tensors="pt").pixel_values
-
-        return {
-            'pixel_values': pixel_values,
-            'labels': torch.tensor(text, dtype=torch.long)
-        }
-
-# Define the Custom Dataset
-class DiffusionTensorDataset(Dataset):
-    def __init__(self, dataframe, root_dir, processor):
-        self.dataframe = dataframe
-        self.root_dir = root_dir
-        self.processor = processor
+# Define a Custom Dataset class for easy testing, later should be replaced by our real dataset
+class AugmentedCIFAR10(Dataset):
+    def __init__(self, root, train=True, transform=None, download=True):
+        self.cifar10 = torchvision.datasets.CIFAR10(root, train=train, download=download)
+        self.transform = transform
 
     def __len__(self):
-        return len(self.dataframe)
+        return len(self.cifar10)
 
     def __getitem__(self, idx):
-        row = self.dataframe.iloc[idx]
-        image_path = os.path.join(self.root_dir, row['preproc_file_name'])
-        text = row['label_eos']
-        return self.processor(image_path, text, max_length=100)
-
-
+        image, label = self.cifar10[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
 
 
 def main():
-    # Load the config file by using the OmegaConf library
+    # Load the config file
     config = OmegaConf.load('../../conf/config.yaml')
 
-    # Initialize the wandb
-    wandb.init(project='diffusion', entity='yecanlee', config=config)
+    # Convert OmegaConf to a plain Python dictionary, this is needed for wandb logging
+    config_dict = OmegaConf.to_container(config, resolve=True)
+
+    # Set up augmentations
+    transform = AlbumentationsTransform([
+        A.RandomCrop(32, 32),
+        A.HorizontalFlip(p=0.5),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    # Create the dataset and data loader
+    dataset = AugmentedCIFAR10(root='./data', train=True, transform=transform, download=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=config.training.batch_size, shuffle=True, num_workers=2)
+
+    # Initialize the model and optimizer
+    eps_pred_model = DiffusionUNet(image_channels=3, n_channels=config.model.n_channels).to(config.device)
+    optimizer = torch.optim.Adam(eps_pred_model.parameters(), lr=config.training.learning_rate)
+
+    # Initialize wandb
+    wandb.init(project='diffusion', config=config_dict)
 
     # Run the training and inference process
-    run(config)
+    run(config, eps_pred_model, optimizer, data_loader)
 
     # Save the model
     torch.save(eps_pred_model.state_dict(), 'eps_pred_model.pth')
 
-#
 if __name__ == '__main__':
     main()
