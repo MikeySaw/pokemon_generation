70bbd3270c0cc72e90e277c78703046356be10a0
Michael Sawitzki <m.sawitzki99@gmail.com>
2024-07-01 13:10:21 +0200

    Final version: moves metadata.jsonl to train/test/val folder

--- src/data/create_data_splits.py
+++ src/data/create_data_splits.py
@@ -3,29 +3,11 @@ import os
 import random
 import shutil
 
-if not os.path.exists("data/processed"):
-    os.makedirs("data/processed")
-
-paths = ['train', 'val', 'test']
-
-
-for path in paths:
-    if not os.path.exists(os.path.join("data/interim", path)):
-        os.makedirs(os.path.join("data/interim", path))
-
-file_path = "data/raw/pokemon_data.json"
-
-with open(file_path, 'r') as file:
-    data = json.load(file)
-
-
 
 def split_data(data: dict, split_ratios: tuple = (0.8, 0.1, 0.1)):
     """
     Based on the json file created in add_data_description.py this function splits data into train, val and test
     """
-    
-    
     random.seed(123)
     n = len(data)
     random.shuffle(data)
@@ -43,15 +25,20 @@ def split_data(data: dict, split_ratios: tuple = (0.8, 0.1, 0.1)):
 
     return train_data, val_data, test_data
 
+
 def move_images(data: dict, destination: str = 'train'):
+    with open(os.path.join("data/interim", destination, 'metadata.jsonl'), "w") as f:
+        for line in data:
+            json.dump(line, f)
+            f.write('\n')
+        
     filenames = [data[i]['file_name'] for i in range(len(data))]
     for file in filenames:
         src_path = os.path.join("data/raw", file)
-        dest_folder = os.path.join("data/interim", f"{destination}")
+        dest_folder = os.path.join("data/interim", destination)
         dest_path = os.path.join(dest_folder, file)
         shutil.copy(src_path, dest_path)
     print(f"Moved all {destination} images to {os.path.join('data/interim', destination)}")
-
 
 
 def main():
@@ -61,6 +48,7 @@ def main():
     move_images(test_data, destination='test')
 
 
+
 if __name__ == "__main__":
    
     if not os.path.exists("data/processed"):
@@ -68,15 +56,14 @@ if __name__ == "__main__":
 
     paths = ['train', 'val', 'test']
 
-
     for path in paths:
         if not os.path.exists(os.path.join("data/interim", path)):
             os.makedirs(os.path.join("data/interim", path))
 
-    file_path = "data/interim/pokemon_data.json"
+    file_path = "data/pokemon_data.json"
 
     with open(file_path, 'r') as file:
         data = json.load(file)
-
+        
     main()
 
